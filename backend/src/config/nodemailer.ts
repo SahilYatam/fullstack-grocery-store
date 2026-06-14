5ed190b1a6e31f66fa989fdf36556114a4335e84
Sahil Yatam <sahilyatam9@gmail.com>
2026-06-14 23:25:32 +0530

    feat(email): add nodemailer configuration and email templates

---
+++ backend/src/config/nodemailer.ts
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+    },
+});
+
+const sendEmail = async ({
+    to,
+    subject,
+    body,
+}: {
+    to: string;
+    subject: string;
+    body: string;
+}) => {
+    const response = await transporter.sendMail({
+        from: process.env.SMTP_USER,
+        to,
+        subject,
+        html: body,
+    });
+    return response;
+};
+
+export default sendEmail;
